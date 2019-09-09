abd0f3184c044225b5db162ed09a94f73c4781af
Xuan Phu MAI <xuanphu.mai@uni.lu>
2019-09-09 11:40:40 +0200

    update singleTest to print the starting and ending time

--- src-test/smrl/mr/singleTest/SingleTest.java
+++ src-test/smrl/mr/singleTest/SingleTest.java
@@ -2,8 +2,11 @@ package smrl.mr.singleTest;
 
 import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,7 +16,11 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import smrl.mr.owasp.OTG_AUTHZ_001b;
 import smrl.mr.owasp.OTG_AUTHZ_002;
 import smrl.mr.owasp.OTG_AUTHZ_002a;
+import smrl.mr.owasp.OTG_AUTHZ_002b;
 import smrl.mr.owasp.OTG_AUTHZ_002c;
+import smrl.mr.owasp.OTG_AUTHZ_002d;
+import smrl.mr.owasp.OTG_AUTHZ_002e;
+import smrl.mr.owasp.OTG_SESS_003;
 import smrl.mr.crawljax.WebOperationsProvider;
 import smrl.mr.language.MRBaseTest;
 
@@ -26,6 +33,8 @@ public class SingleTest extends MRBaseTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		
+		System.out.println("*** Starting time: " + getCurrentTime() +" ***");
 		
 		//by default, the SUT is the Jenkins
 		String configFile = "./testData/Jenkins/jenkinsSysConfig.json";
@@ -46,6 +55,10 @@ public class SingleTest extends MRBaseTest {
 		setProvider(provider);
 	}
 	
+	@AfterClass
+    public static void printEndingTime() {
+		System.out.println("*** Ending time: " + getCurrentTime() + " ***");
+    }   
 	
 	@Test
 	public void test_OTG_AUTHZ_001b() {
@@ -66,14 +79,14 @@ public class SingleTest extends MRBaseTest {
 		super.test(provider,OTG_AUTHZ_002a.class);
 	}
 	
-//	@Test
-//	/**
-//	  *This test case should detect CVE-2018-1999003"
-//	  */
-//	public void test_OTG_AUTHZ_002b() {
-//		super.test(provider,OTG_AUTHZ_002b.class);
-//	}
-//	
+	@Test
+	/**
+	  *This test case should detect CVE-2018-1999003"
+	  */
+	public void test_OTG_AUTHZ_002b() {
+		super.test(provider,OTG_AUTHZ_002b.class);
+	}
+	
 	@Test
 	/**
 	  *This test case should detect CVE-2018-1999046"
@@ -82,27 +95,35 @@ public class SingleTest extends MRBaseTest {
 		super.test(provider,OTG_AUTHZ_002c.class);
 	}
 	
-//	@Test
-//	/**
-//	  *This test case should detect CVE-2018-1000406"
-//	  */
-//	public void test_OTG_AUTHZ_002d() {
-//		super.test(provider,OTG_AUTHZ_002d.class);
-//	}
-//	
-//	@Test
-//	public void test_OTG_INPVAL_004() {
-//		super.test(provider,OTG_INPVAL_004.class);
-//	}
-//	
-//	@Test
-//	/**
-//	  *This test case should detect CVE-2018-1000409"
-//	  */
-//	public void test_OTG_SESS_003() {
-//		super.test(provider,OTG_SESS_003.class);
-//	}
-//	
+	@Test
+	/**
+	  *This test case should detect CVE-2018-1000406"
+	  */
+	public void test_OTG_AUTHZ_002d() {
+		super.test(provider,OTG_AUTHZ_002d.class);
+	}
+	
+	@Test
+	/**
+	  *This test case should detect CVE-2018-1999003"
+	  */
+	public void test_OTG_AUTHZ_002e() {
+		super.test(provider,OTG_AUTHZ_002e.class);
+	}
+	
+	@Test
+	public void test_OTG_INPVAL_004() {
+		super.test(provider,OTG_INPVAL_004.class);
+	}
+	
+	@Test
+	/**
+	  *This test case should detect CVE-2018-1000409"
+	  */
+	public void test_OTG_SESS_003() {
+		super.test(provider,OTG_SESS_003.class);
+	}
+	
 	@Test
 	public void test_cover_urls_u2_u1() {
 		assertTrue(provider.coverAllUrls("user2", "user1"));
@@ -139,5 +160,11 @@ public class SingleTest extends MRBaseTest {
 		
 		driver.get("http://192.168.56.102:8080");
 		driver.get("/");
+	}
+
+	public static String getCurrentTime() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();  
+		return dtf.format(now).toString();  
 	}
 }
